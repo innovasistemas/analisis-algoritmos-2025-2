dc1dd975caa9389f377c9d99f60b208289ca2856
unknown <innovasistemas@gmail.com>
2025-08-14 09:52:38 -0500

    Primeros ejemplos Java y Python

---
+++ HelloWorldJava/HelloWorld.java
@@ -0,0 +1,17 @@
+package HelloWorldJava;
+
+public class HelloWorld 
+{
+    public static void main(String args [])
+    {
+        String name = "Ana";
+        String lastName = " Gil";
+        String fullName = name.concat(lastName);
+        System.out.println("Hola, esto es Java");
+        System.out.println("Nombre: " + fullName);
+        boolean x = true;
+        System.out.println("x: " + x);
+
+        
+    }
+}
---
+++ hello_world_python/hello_world.py
@@ -0,0 +1,19 @@
+name = "Ana"
+lastName = " Gil"
+fullName = name + lastName
+print("Hola, esto es Python")
+print("Nombre: " + fullName)
+print(f"Su nombre interpolado es {fullName}")
+
+if "A" < "a":
+    print("A mayúscula es menor que a minúscula")
+else:
+    print("a mayúscula es menor que A minúscula")
+
+if "z" < "a":
+    print("z es menor que a")
+else:
+    print("a es menor que z")
+
+x = False
+print(f"x={x}")
